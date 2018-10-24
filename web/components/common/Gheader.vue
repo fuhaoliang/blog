4c55fed0db665a6783d4ff5ac778240dad3ec8fe
付豪亮 <358072810@qq.com>
2018-10-24 09:38:04 +0800

    Web-blog init

---
+++ web/components/common/Gheader.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+<script>
+export default {
+  name: 'Gheader',
+  data() {
+    return {
+      msg: '我是头部'
+    }
+  }
+}
+</script>
+<style lang="scss">
+h1 {
+  color: red;
+  transform: scale(1.5);
+  p {
+    color: blue;
+  }
+}
+</style>
