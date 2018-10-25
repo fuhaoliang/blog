a75a03eedbcbd26c8eae0b496274bb28375657af
fuhaoliang <fuhaoliang@juran.com.cn>
2018-10-25 20:50:56 +0800

    header

--- web/components/common/Gheader.vue
+++ web/components/common/Gheader.vue
@@ -1,22 +1,91 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="g-header">
+    <header class="header-max">
+      <img class="logo" src="../../assets/image/common/logo_gif.gif" alt="">
+      <div class="nav">
+        <ul class="nav-l">
+          <li>
+            Home
+            <i class="underline-icon"></i>
+          </li>
+          <li>Mobile</li>
+          <li>Gellery</li>
+          <li>Learn</li>
+          <li>Blog</li>
+          <li>About</li>
+        </ul>
+        <ul class="nav-r">
+          <li>Sign In</li>
+          <li>Join Now</li>
+          <li>Language</li>
+        </ul>
+      </div>
+    </header>
+  </div>
 </template>
-<script>
-export default {
-  name: 'Gheader',
-  data() {
-    return {
-      msg: '我是头部'
+
+<style lang="less">
+@import url('../../assets/css/common');
+.g-header {
+  .header-max {
+    position: fixed;
+    top: 0;
+    right: 0;
+    display: flex;
+    width: 100%;
+    height: 54px;
+    padding: 0 30px;
+    .logo {
+      .imgFull;
+      width: 90px;
     }
-  }
-}
-</script>
-<style lang="scss">
-h1 {
-  color: red;
-  transform: scale(1.5);
-  p {
-    color: blue;
+    .nav {
+      font-size: 14px;
+      .nav-l {
+        display: flex;
+        li {
+          display: block;
+          width: 100px;
+          color: #fff;
+          text-align: center;
+          line-height: 54px;
+        }
+      }
+      .nav-r {
+        position: absolute;
+        top: 0;
+        right: 0;
+        display: flex;
+        li {
+          display: block;
+          width: 100px;
+          color: #fff;
+          text-align: center;
+          line-height: 54px;
+        }
+      }
+      li {
+        position: relative;
+        &::after {
+          content: '';
+          position: absolute;
+          top: 45px;
+          left: 0;
+          right: 0;
+          margin: 0 auto;
+          display: block;
+          width: 0;
+          height: 1px;
+          background: #ffffff;
+          transition: width 0.3s;
+        }
+        &:hover {
+          &::after {
+            width: 70px;
+          }
+        }
+      }
+    }
   }
 }
 </style>
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,24 +1,6 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        web
-      </h1>
-      <h2 class="subtitle">
-        blogWeb
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+
   </section>
 </template>
 
@@ -35,6 +17,8 @@ export default {
 <style>
 .container {
   min-height: 100vh;
+  height: 1000px;
+  background-color: blue;
   display: flex;
   justify-content: center;
   align-items: center;
